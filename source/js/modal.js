21b3dbb781e3ba82f7c08927676b0e46b7a26631
Richie <mari4orl@mail.ru>
2020-05-06 19:07:05 +0300

    Готовый к сдаче 1

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,26 +1,36 @@
-var cartIcon = document.querySelectorAll('.order');
-var modal = document.querySelector('.modal');
-var overlay = document.querySelector('.modal__overlay');
-var addBtn = document.querySelector('.modal__add');
+var cartIconElems = document.querySelectorAll('.order'); // Название переменной во множественном числе, нам массив елементов возвращается
+var modalElem = document.querySelector('.modal');
+var overlayElem = document.querySelector('.modal__overlay');
+var addBtnElem = document.querySelector('.modal__add');
+
+var ESC_KEYCODE = 27
+
+
+function onEscKeyDown(e) {
+  if(e.keyCode === ESC_KEYCODE) {
+    modalElem.classList.add('modal--closed');
+  }
+}
 
 function openModal() {
-  if (modal.classList.contains('modal--closed')) {
-    modal.classList.remove('modal--closed');
+  if (modalElem.classList.contains('modal--closed')) {
+    modalElem.classList.remove('modal--closed');
+    document.addEventListener('keydown', onEscKeyDown)
   }
 }
 
 function closeModal() {
-  modal.classList.add('modal--closed');
-}
-
-for (var i = 0; i < cartIcon.length; i++) {
-  cartIcon[i].addEventListener('click', openModal);
-}
-
-addBtn.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-document.addEventListener('keydown', function (e) {
-  if(e.keyCode === 27) {
-    modal.classList.add('modal--closed');
+  if (!modalElem.classList.contains('modal--closed')) {
+    modalElem.classList.add('modal--closed');
+    document.removeEventListener('keydown', onEscKeyDown)
   }
-});
+}
+
+
+for (var i = 0; i < cartIconElems.length; i++) {
+  var cartIconElem = cartIconElems[i];
+  cartIconElem.addEventListener('click', openModal);
+}
+
+addBtnElem.addEventListener('click', closeModal);
+overlayElem.addEventListener('click', closeModal);
